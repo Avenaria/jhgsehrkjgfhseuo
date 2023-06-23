0f2edbeaa526070ebdefc3bfa5e5aa706edce21a
Александр Гурьев <arh-tehnik@mail.ru>
2023-06-23 20:18:02 +0300

    Добавьте файлы проекта.

---
+++ jhgsehrkjgfhseuo/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+//namespace jhgsehrkjgfhseuo
+//{
+//    internal class  OperTime:IDisposable
+//    {
+//        long start1;
+//        string texst;
+//            int count;
+//        public OperTime(string t)
+//        {
+//            texst = t;
+//            count = GC.CollectionCount(0);
+//            start1 = Stopwatch.GetTimestamp(0);
+
+
+//        }
+
+//        public void Dispose()
+//        {
+//            Console.WriteLine($"{texst}");
+
+//        }
+//    }
---
+++ jhgsehrkjgfhseuo/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.Threading.Tasks;
+
+namespace jhgsehrkjgfhseuo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+//обопщеные кол
+
+            //list<> 
+            //SortedList -сразу сортирует данные что внсятся
+            //Queue<>
+            //Distionary -ключи и его значени ключ уникалный, значение любое
+
+            //List<int> ints = new List<int>();
+            //ints.Add(1);
+            //ints.Add(6);
+            //Console.WriteLine(ints[2]);
+            //const int COUNT = 100_0000;
+            //using (new OperTime("List"))
+            //{
+            //     List<int> l= new List<int>(COUNT);
+            //    for(int i = 0; i< COUNT; i++)
+            //    {
+            //        l.Add(i);
+            //        int x = ints[i];
+                    
+            //    }
+            //    ints = null;
+            //}
+
+            //using (new OperTime("ArrayList"))
+            //{
+            //    ArrayList A = new ArrayList(COUNT);
+            //    for (int i = 0; i < COUNT; i++)
+            //    {
+            //        A.Add(i);
+            //        int x = (int)A[i];
+
+            //    }
+            //    A = null;
+            //}
+            //Dictionary<string, int> dic = new Dictionary<string, int>();///первый тип данных для ключа второй для значения
+            //dic["gr1"] = 12;
+            //dic.Add("gr2", 45);
+            //dic.Add("gr3", 5);
+            //dic.Add("gr4",69);
+            //dic.Add("gr5", 96);
+            //foreach (KeyValuePair<string, int> pair in dic)
+            //{
+            //    Console.WriteLine(pair.Key);
+            //}
+
+            //dic.Remove("gr4");//удаление 
+
+
+
+        }
+    }
+}
